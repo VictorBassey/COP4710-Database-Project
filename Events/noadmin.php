870d0c96fba8dd1fbd2cd6d22286b70ab876c7f7
xD12 <george_45a@hotmail.com>
2017-04-11 21:02:02 -0400

    no admin error

---
+++ Events/noadmin.php
@@ -0,0 +1,12 @@
+<div>
+<link rel="stylesheet" type="text/css"
+    href="../Events/eventstyle.css">
+</div>
+<?php 
+echo'Sorry, but Your not an admin of any RSO'; 
+?>
+<br>
+<br>
+<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="true"><a href="public_event.php" style="text-decoration: none">Go back To View Events</a>
+        <span class="caret"></span> 
+        </button>
