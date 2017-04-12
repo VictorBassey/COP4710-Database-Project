71ab7433b72a15fc6c0d2c7945fc61a213e8ba51
xD12 <george_45a@hotmail.com>
2017-04-11 21:02:46 -0400

    rso events

--- Events/rso_event.php
+++ Events/rso_event.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <?php 
+ <?php 
     session_start();
     include'../navbar/navbar.php';
     include'../navbar/includes/dbh.php';
@@ -22,6 +22,16 @@
   <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
     </head>
     
+    <link rel="stylesheet" type="text/css"
+    href="../Events/eventstyle.css">
+    
+     <p>Date/Time: <span id="datetime"></span></p>
+
+    <script>
+        var dt = new Date();
+        document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
+    </script>
+    
 <body>
     <center><h3>View Events</h3></center>
     
@@ -30,19 +40,11 @@
         <a href="otherevents.php"?id='uni'><input type="submit" name="search" value="Filter"></a><br><br>
     </form>
     
-    <div class="date">
-        <p id="date"></p>
-        <script>
-        document.getElementById("date").innerHTML = Date();
-        </script>
-    </div>
-    
     <div class="dropdown"> 
         <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="true"> View By... 
         <span class="caret"></span> 
         </button> 
         <ul class="dropdown-menu" role="menu" style="overflow-y: hidden;"> 
-            <li class="active"> <a href="/">Day View</a></li>
             <li> <a href="public_event.php">public</a> </li> 
             <li> <a href="rso_event.php">RSO</a> </li> 
             <li> <a href="priv_event.php">Private</a> </li> 
@@ -51,13 +53,21 @@
     
 <div class="container">
 <?php       
-
-
-echo '<br><center><label>Your RSO events</label>';
     
-$uid = $_SESSION['uid']; 
+   //checked if logged in
+    if(!isset($_SESSION['username'])) {
+      echo '<h1>Sorry, you must be logged in to view this event.</h1>';
+      echo '</div></div></body></html>';
+      die();
+    }
+
+echo'<br><center><h1>RSO Events</h1>';
+
+$username = $_SESSION['username'];
     
-$sql = "SELECT * FROM events WHERE eventtype = 'rso' AND rsoid IN (SELECT rsoid FROM memberof WHERE uid = '$uid')";
+$uid = "SELECT uid FROM userWHERE name = '$username'";
+    
+$sql = "SELECT * FROM events WHERE eventtype = 'rso' AND rsoid IN (SELECT rsoid FROM memberof WHERE uid='$uid')";
 
 $result = $mysqli->query($sql);
     
@@ -65,14 +75,12 @@ $result = $mysqli->query($sql);
     {
         $row = mysqli_fetch_assoc($result);
         
-        echo '<tr><br>You have no RSOs</br><br><button type="submit" name="join"><a href="../RSO/rso_create.php?id='.$uid.'">Create RSOs</a></button></tr><br>'; 
-        
-        echo '<tr><br><button type="submit" name="join"><a href="../RSO/rso_join.php?id='.$uid.'">Join RSOs</a></button></tr>'; 
+        echo '<tr><br><button type="submit" name="join"><a href="rso_join.php">Join RSOs</a></button></tr>'; 
     }
-    else 
-    {   
-        echo '<button type="submit" name="join"><a href="create_rso.php">Create RSO</a></button></tr>'; 
+    else
+    {
     ?>
+    
     </div>
     
         <table class="table table-bordered table-hover myeventtable">
@@ -83,7 +91,6 @@ $result = $mysqli->query($sql);
 				<th>Type</th>
 				<th>Location</th>
 				<th>Time</th>
-                <th>Join</th>
                 <th>Display</th>
 			</tr>
             </div>
@@ -96,8 +103,7 @@ $result = $mysqli->query($sql);
                 <td>'.$row['eventtype'].'</td>
                 <td>'.$row['location'].'</td>
                 <td>'.$row['time'].'</td>
-                <td><h3><a href="join_rso.php?varname='.$row['eid'].'">Join</a></h3></td>
-                <td><h3><a href="displayEvent.php?varname='.$row['eid'].'">Display</a></h3></td></tr>';
+                <td><h3><a href="event_comments.php?varname='.$row['eid'].'">Display</a></h3></td></tr>';
                 echo'</table>'; 
             }
         ?>
