87d8c1c80f5a47b9152f2c7e74e7452928bdb991
xD12 <george_45a@hotmail.com>
2017-04-11 21:02:26 -0400

    other universities public events

--- Events/otherevents.php
+++ Events/otherevents.php
@@ -22,6 +22,16 @@
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
   <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
     </head>
+
+   <link rel="stylesheet" type="text/css"
+    href="../Events/eventstyle.css">
+    
+      <p>Date/Time: <span id="datetime"></span></p>
+
+    <script>
+        var dt = new Date();
+        document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
+    </script>
     
 <body>
     <center><h3>View Events</h3></center>
@@ -33,19 +43,11 @@
         </form>
     </div>
     
-    <div class="date">
-        <p id="date"></p>
-        <script>
-        document.getElementById("date").innerHTML = Date();
-        </script>
-    </div>
-    
     <div class="dropdown"> 
         <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="true"> View By... 
         <span class="caret"></span> 
         </button> 
         <ul class="dropdown-menu" role="menu" style="overflow-y: hidden;"> 
-            <li class="active"><a href="/">Day View</a></li>
             <li> <a href="public_event.php">public</a> </li> 
             <li> <a href="rso_event.php">RSO</a> </li> 
             <li> <a href="priv_event.php">Private</a> </li> 
@@ -55,14 +57,12 @@
 
 <?php 
 
-
 echo'<br>';
     
+//not need to be logged in here
  $univname = $mysqli->real_escape_string($_POST['uni']);
 
     $sql = "SELECT * FROM events WHERE eventtype = 'public' AND location = '$univname'";
-
-    //$sql = "SELECT * FROM events WHERE eventtype = 'public' AND rsoid IN (SELECT rsoid FROM rsoaffiliation WHERE univid IN (SELECT univid FROM university WHERE name = '$univname'))"; 
 
     $result = $mysqli->query($sql); 
     
@@ -92,7 +92,7 @@ echo'<br>';
                 <td>'.$row['eventtype'].'</td>
                 <td>'.$row['location'].'</td>
                 <td>'.$row['time'].'</td>
-                <td><h3><a href="displayEvent.php?varname='.$row['eid'].'">Display Event</a></h3></td></tr>';
+                <td><h3><a href="event_comments.php?varname='.$row['eid'].'">Display</a></h3></td></tr>';
                 echo'</table>'; 
         } 
     ?>
